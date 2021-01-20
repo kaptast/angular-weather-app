2dc783f0283ff2b16b435de6a098287c3e3ef202
kaptast <kaptas.tamas@gmail.com>
2021-01-20 11:52:54 +0100

    configure runtime appconfig

---
+++ src/app/app-config.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppConfigService {
+  private appConfig: any;
+
+  constructor(private http: HttpClient) { }
+
+  // Loads the config.json
+  // Runs at app initialization
+  loadAppConfig() {
+    return this.http.get('/assets/config.json')
+      .toPromise()
+      .then(data => {
+        console.info('Config loaded!');
+        this.appConfig = data;
+      });
+  }
+
+  // Gets the Base API url set in config.json
+  get weatherApiKey() {
+    if (!this.appConfig) {
+      throw Error('Config file not loaded!');
+    }
+
+    return this.appConfig.weatherApiKey;
+  }
+}
