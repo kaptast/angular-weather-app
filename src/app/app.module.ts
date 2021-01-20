bb7c81e00ded2b5ebee8c35eccb4224bdec77f3c
kaptast <kaptas.tamas@gmail.com>
2021-01-20 11:53:22 +0100

    configure app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CityComponent } from './city/city.component';
 import { CityListComponent } from './city-list/city-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './material/material.module';
+import { AppConfigService } from './app-config.service';
+import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +20,30 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      deps: [
+        AppConfigService,
+        AuthService
+      ],
+      useFactory: (
+        appConfigService: AppConfigService,
+        authService: AuthService
+      ) => {
+        return() => {
+          return appConfigService.loadAppConfig()
+            .then(() => authService.checkLogin());
+        }
+      }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
