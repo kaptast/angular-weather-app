6f82c051ab54f47a407b54c145f03c7d05b23aba
kaptast <kaptas.tamas@gmail.com>
2021-01-21 01:48:14 +0100

    city service

--- src/app/city.service.ts
+++ src/app/city.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { City } from './city';
+import { DatabaseService } from './database.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CityService {
 
-  constructor() { }
+  constructor(
+    private dbService: DatabaseService,
+    private authService: AuthService
+  ) { }
+
+  getCities(): Observable<City[]> {
+    const userid = this.authService.getUserId();
+    console.log(userid);
+    return this.dbService.getCities(userid);
+  }
+
+  addCity(city: City): Observable<City> {
+    const userid = this.authService.getUserId();
+    city.userid = userid;
+    return this.dbService.addCity(city);
+  }
+
 }
