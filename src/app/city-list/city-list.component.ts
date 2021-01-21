f2382bfa8435f24a4ab71e9384e8337f51105c1a
kaptast <kaptas.tamas@gmail.com>
2021-01-21 01:49:48 +0100

    city list component

--- src/app/city-list/city-list.component.ts
+++ src/app/city-list/city-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { City } from '../city';
+import { CityService } from '../city.service';
 
 @Component({
   selector: 'app-city-list',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./city-list.component.css']
 })
 export class CityListComponent implements OnInit {
+  cities: City[];
 
-  constructor() { }
+  constructor(
+    private cityService: CityService,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.getCities();
   }
 
+  private getCities(): void {
+    this.cityService.getCities()
+      .subscribe(cities => {
+        this.cities = cities;
+      });
+  }
+
+  addCity(cityname: string): void {
+    cityname = cityname.trim();
+    if (!cityname) {
+      return;
+    }
+
+    this.cityService.addCity({ cityname } as City)
+      .subscribe(_ => {
+        console.log('city added');
+        this.getCities();
+      })
+  }
+
+  logout(): void {
+    this.authService.logout().subscribe();
+  }
 }
