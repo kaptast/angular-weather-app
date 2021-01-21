99db1eadd78c07b93fe84e9ea815f2498a6a371a
kaptast <kaptas.tamas@gmail.com>
2021-01-21 01:48:30 +0100

    login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { User } from '../user';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(
+    private authService: AuthService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  getIsLoggedIn() {
+    return this.authService.isLoggenIn;
+  }
+
+  login(username: string, password: string) {
+    this.authService.login(username, password);
+  }
+
+}
